2571ca60e318e9e8ce80b0741796d0885ebffceb
Jia Yang <jche0374@student.monash.edu>
2023-10-13 00:46:03 +0800

    html done
    TODO: prettify + add flavor text

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "js/happiness_choropleth_map.vg.json";
+vegaEmbed("#happiness_choropleth_map", vg_1)
+  .then(function (result) {})
+  .catch(console.error);
+
+var vg_2 = "js/crime_choropleth_map.vg.json";
+vegaEmbed("#crime_choropleth_map", vg_2)
+  .then(function (result) {})
+  .catch(console.error);
+
+var vg_3 = "js/rank_bar_chart.vg.json";
+vegaEmbed("#rank_bar_chart", vg_3)
+  .then(function (result) {})
+  .catch(console.error);
+ 
+var vg_4 = "js/rank_crime_bar_chart.vg.json";
+vegaEmbed("#rank_crime_bar_chart", vg_4)
+  .then(function (result) {})
+  .catch(console.error);
+
+var vg_5 = "js/other_factors_stackedbar.vg.json";
+vegaEmbed("#other_factors_stackedbar", vg_5)
+  .then(function (result) {})
+  .catch(console.error);
+
+var vg_6 = "js/gdp_bubbleplot.vg.json";
+vegaEmbed("#gdp_bubbleplot", vg_6)
+  .then(function (result) {})
+  .catch(console.error);
--- map.js
+++
@@ -1,6 +0,0 @@
-var vg_1 = "https://raw.githubusercontent.com/CheongJiaYang/3179Week9Homework/main/map.json";
-vegaEmbed("#map", vg_1).then(function(result) {
-
-
-}).catch(console.error);
-
